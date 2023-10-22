5dd5225ae66adf3930a621cc1027d46c9b2e15a7
butavat <139517746+butavat@users.noreply.github.com>
2023-10-22 18:36:57 -0400

    task 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,16 @@
-const name = 'Tetiana';
-const age = 43;
-const subject = 'JavaScript';
-let message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${subject}`;
-console.log(message);
+// const name = 'Tetiana';
+// const age = 43;
+// const subject = 'JavaScript';
+// let message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${subject}`;
+// console.log(message);
 
+// ЗАДАЧА 2
+// ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
+
+const userName = prompt("Введіть своє ім'я");
+const userMail = prompt('Введіть вашу пошту');
+const userPhone = prompt('Введіть ваш номер телефону');
+
+let message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${userMail} ПОЧТУ І ${userPhone} ТЕЛЕФОН`;
+console.log(message);
+alert();
