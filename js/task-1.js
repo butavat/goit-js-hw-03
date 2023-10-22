2b9c5c074db8326a3416195074d2ec5901dcae90
butavat <139517746+butavat@users.noreply.github.com>
2023-10-22 18:01:00 -0400

    task 1

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const name = 'Tetiana';
+const age = 43;
+const subject = 'JavaScript';
+let message = `ДОБРОГО ДНЯ,МЕНЕ ЗВАТИ ${name}, МЕНІ ${age} РОКІВ, Я ВИВЧАЮ ${subject}`;
+console.log(message);
+
