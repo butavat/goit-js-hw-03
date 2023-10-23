50130974a80487f767b4a507e58bcb166efc7aeb
butavat <139517746+butavat@users.noreply.github.com>
2023-10-23 16:26:29 -0400

    task3

--- js/task-1.js
+++ js/task-1.js
@@ -7,10 +7,21 @@
 // ЗАДАЧА 2
 // ЗАПИТАТИ У КОРИСТУВАЧА ІМЯ, ПОЧТУ , ТЕЛЕФОН, І ПОКАЗАТИ У КОНСОЛІ "КОРИСТУВАЧ __ ВИКОРИСТОВУЄ  __ ПОЧТУ І __ ТЕЛЕФОН"
 
-const userName = prompt("Введіть своє ім'я");
-const userMail = prompt('Введіть вашу пошту');
-const userPhone = prompt('Введіть ваш номер телефону');
+// const userName = prompt("Введіть своє ім'я");
+// const userMail = prompt('Введіть вашу пошту');
+// const userPhone = prompt('Введіть ваш номер телефону');
 
-let message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${userMail} ПОЧТУ І ${userPhone} ТЕЛЕФОН`;
-console.log(message);
-alert();
+// let message = `КОРИСТУВАЧ ${userName} ВИКОРИСТОВУЄ ${userMail} ПОЧТУ І ${userPhone} ТЕЛЕФОН`;
+// console.log(message);
+// alert(message);
+
+//ЗАДАЧА 3
+// ПОРАХУЙТЕ СУМУ ТОВАРІВ У КОРЗИНІ . ПОТРІБНО ВВЕСТИ СУМУ 3-Х ТОВАРІВ І ПОРАХУВАТИ ЇХ СУМУ
+
+// const mobile = Number(prompt("Введіть ціну телефона"));
+// const simCard = Number(prompt("Введіть ціну sim-картки"));
+// const caseMobile = +prompt("Введіть ціну чохла на телефон");
+// let sum = mobile + simCard + caseMobile;
+
+// console.log(sum);
+// alert(sum)
